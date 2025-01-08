e91f617e6e702446cb8289f8d6ddd3c7daec521d
Ahmet Burkay KINIK <aburkaykinik@gmail.com>
2025-01-08 23:59:05 +0300

    Runs new codes now

--- connect_to_container.sh
+++ connect_to_container.sh
@@ -7,19 +7,24 @@ tmux split-window -v
 tmux select-pane -t 0
 # Split the window vertically (side by side)
 tmux split-window -h
+tmux select-pane -t 1
+tmux split-window -h
 
 tmux set -g mouse
 # Send the command to the left pane
 tmux send-keys -t 0 "docker exec -it nat_container1-peer-1 bash" C-m
+tmux send-keys -t 1 "docker exec -it nat_container2-peer-1 bash" C-m
 
 # Send the command to the right pane
-tmux send-keys -t 1 "docker exec -it public_container1-server-1 bash" C-m
+tmux send-keys -t 2 "docker exec -it public_container1-server-1 bash" C-m
+tmux send-keys -t 3 "docker exec -it public_container1-server-1 bash" C-m
 
-tmux send-keys -t 2 "docker exec -it nat_container1-nat-1 bash" C-m
 
+tmux send-keys -t 0 "python3 src/peer/peer.py --no-relay" C-m
+tmux send-keys -t 1 "python3 src/peer/peer.py --no-relay" C-m
 
-tmux send-keys -t 1 "python3 python_codes/udp_server.py" C-m
-tmux send-keys -t 0 "python3 python_codes/udp_client.py 15.0.0.2 5005" C-m
+tmux send-keys -t 2 "python3 src/server/server.py" C-m
+tmux send-keys -t 3 "python3 src/peer/peer.py --relay" C-m
 
 
 # Attach to the tmux session
